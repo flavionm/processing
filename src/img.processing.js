2dc4326834e73394420d01c9fcc2db2d14e21dc8
Flávio Sousa <fn_maia@outlook.com>
2021-05-11 19:44:51 -0300

    Add sobel filtering

--- src/img.processing.js
+++ src/img.processing.js
@@ -24,6 +24,8 @@
             const filter_size = filter_padding + 1;
 
             const laplace_filter = nj.int8([[0, -1, 0], [-1, 4, -1], [0, -1, 0]]);
+            const sobel_right_filter = nj.int8([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+            const sobel_down_filter = nj.int8([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
 
             if (border === 'extend') {
                 const extended = nj.zeros([this.height + filter_padding, this.width + filter_padding, 4], 'uint8')
@@ -61,6 +63,25 @@
                         for (var k = 0; k < 4; k++) {
                             new_img.set(y, x, k, result.get(k));
                         }
+                    } else if (this.kernel === 'sobel') {
+                        var result_x = 0;
+                        var result_y = 0;
+
+                        for (var i = 0; i < filter_size; i++) {
+                            for (var j = 0; j < filter_size; j++) {
+                                const gray = 0.299 * filter.get(i, j, 0) + 0.587 * filter.get(i, j, 1) + 0.114 * filter.get(i, j, 2);
+                                result_x += gray * sobel_right_filter.get(i, j) / 8;
+                                result_y += gray * sobel_down_filter.get(i, j) / 8;
+                            }
+                        }
+
+                        var result = 2 * Math.abs(Math.sqrt(result_x ** 2 + result_y ** 2));
+
+                        for (var k = 0; k < 3; k++) {
+                            new_img.set(y, x, k, result);
+                        }
+                        new_img.set(y, x, k, 255);
+
                     } else if (this.kernel === 'laplace') {
                         var result = 0;
 
